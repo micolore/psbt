0195ddee4f92c65795454d127d96b1512e1c400e
kubrick <471304402@qq.com>
2021-09-14 11:13:16 +0800

    20210914-1

--- p-docker.pl
+++ p-docker.pl
@@ -4,4 +4,15 @@ use Getopt::Std;
 use vars qw($opt_n);
 getopts('n:');
 print "\$opt_n =>; $opt_n\n" if $opt_n;
-system("docker exec -it " . $opt_n ." sh");
+
+%data = ('1','mysql');
+
+@keys = keys %data;
+
+foreach $k(@keys){
+    if($k == $opt_n){
+      $docker_name = %data{$k};	
+	  print "docker_name: $docker_name \n"; 
+    } 
+}
+system("docker exec -it " . $docker_name ." sh");
